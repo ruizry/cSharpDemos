1f08278f1697224d49203758821a6e29a30d8def
RyanR <ruizry@oregonstate.edu>
2018-07-26 13:12:53 -0700

    Added string demo

---
+++ cSharpStringDemo/cSharpStringDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpStringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            string catdemo = "";
+            StringBuilder paragraph = new StringBuilder();
+            StringBuilder sentence = new StringBuilder();
+            StringBuilder exit = new StringBuilder();
+            exit.Append("exit");
+
+            Console.WriteLine("Please enter 3 strings to concatenate. The second will be entered as all caps.");
+            while (i <= 3)
+            {
+                Console.WriteLine("Please enter string" + i + ":");
+                if (i == 2)
+                {
+                    catdemo += Console.ReadLine().ToUpper();
+                }
+                else
+                {
+                    catdemo += Console.ReadLine();
+                }
+                i++;
+            }
+            Console.WriteLine("Final string after concatenation: " + catdemo);
+
+            Console.WriteLine("Paragraph builder: Enter 1 sentence at a time");
+            Console.WriteLine("Enter a sentence or type exit to finish");
+            sentence.Append(Console.ReadLine());
+            while (!sentence.Equals(exit))
+            {
+                paragraph.Append(sentence);
+                paragraph.Append(" ");
+                sentence.Clear();
+                Console.WriteLine("Enter a sentence or type exit to finish");
+                sentence.Append(Console.ReadLine());
+            }
+            Console.WriteLine("Resulting paragraph:");
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cSharpWhileDemo/cSharpWhileDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpWhileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to be mulitplied by 4 5 times:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            bool repeat = true;
+            string choice = "true";
+            while (i <= 5)
+            {
+                number *= 4;
+                Console.WriteLine("Step " + i + ": " + number);
+                i++;
+            }
+            do
+            {
+                Console.WriteLine("The current value is " + number);
+                Console.WriteLine("Would you like to double this number? (yes or true to double)");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "yes":
+                        number *= 2;
+                        repeat = true;
+                        break;
+                    case "true":
+                        number *= 2;
+                        repeat = true;
+                        break;
+                    default:
+                        repeat = false;
+                        break;
+                }  
+
+            }
+            while (repeat == true);
+        }
+    }
+}
