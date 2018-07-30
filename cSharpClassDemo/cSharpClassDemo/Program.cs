ca3eb0b644a6b6005f6ee0fb83a3d85372f0be03
RyanR <ruizry@oregonstate.edu>
2018-07-30 12:39:16 -0700

    Added first demo for class

---
+++ cSharpClassDemo/cSharpClassDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userinput = 0;
+            SimpleMathClass mathinstance = new SimpleMathClass();
+            Console.WriteLine("Please enter number to test addTwo method");
+            userinput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The result of " + userinput + " being plugged into the addTwo method: " + mathinstance.addTwo(userinput));
+
+            Console.WriteLine("\nPlease enter number to test subtractTen method");
+            userinput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The result of " + userinput + " being plugged into the addTwo method: " + mathinstance.subtractTen(userinput));
+
+            Console.WriteLine("\nPlease enter number to test multipleByThree method");
+            userinput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The result of " + userinput + " being plugged into the addTwo method: " + mathinstance.multipleByThree(userinput));
+
+            Console.WriteLine("\nEnd of the program, press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cSharpClassDemo/cSharpClassDemo/SimpleMathClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpClassDemo
+{
+    class SimpleMathClass
+    {
+        public int addTwo(int num)
+        {
+            return num + 2;
+        }
+        public int multipleByThree(int num)
+        {
+            return num * 3;
+        }
+        public int subtractTen(int num)
+        {
+            return num - 10;
+        }
+    }
+}
