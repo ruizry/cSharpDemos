a16fc347c1a0b272db6422ee2cbc734420adbeb7
RyanR <ruizry@oregonstate.edu>
2018-07-31 15:56:40 -0700

    Added second method demo

---
+++ cSharpMethodDemo2/cSharpMethodDemo2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpMethodDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userinput1 = 0;
+            int userinput2 = 0;
+
+            multiplyTwoNumClass mathinstance = new multiplyTwoNumClass();
+
+            Console.WriteLine("Please enter the first number to use for this multiplication method");
+            userinput1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number to use for this multiplication method");
+            userinput2 = Convert.ToInt32(Console.ReadLine());
+
+            mathinstance.multiplyTwo(num1: userinput1, num2: userinput2);
+            Console.WriteLine(userinput1 + " multiplied by " + userinput2 + " is: " + mathinstance.result);
+
+            Console.WriteLine("\nEnd of the program, press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cSharpMethodDemo2/cSharpMethodDemo2/multiplyTwoNumClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpMethodDemo2
+{
+    class multiplyTwoNumClass
+    {
+        public int result = 0;
+
+        public void multiplyTwo (int num1, int num2)
+        {
+            result = num1 * num2;
+        }
+    }
+}
