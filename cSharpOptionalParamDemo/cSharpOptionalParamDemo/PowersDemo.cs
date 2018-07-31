8526957d0e2485976b45729de43bf281b7068820
RyanR <ruizry@oregonstate.edu>
2018-07-31 11:50:05 -0700

    added third class demo

---
+++ cSharpOptionalParamDemo/cSharpOptionalParamDemo/PowersDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpOptionalParamDemo
+{
+    class PowersDemo
+    {
+        public double powers(double num1, double num2 = 1)
+        {
+            return Math.Pow(num1, num2);
+        }
+    }
+}
---
+++ cSharpOptionalParamDemo/cSharpOptionalParamDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpOptionalParamDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userinput1 = 0;
+            string stringinput2 = "";
+            int userinput2 = 0;
+
+            PowersDemo mathinstance = new PowersDemo();
+            Console.WriteLine("Please enter the first number that will be passed as an integer to the powers method (this input is required)");
+            userinput1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number that will be passed as an integer to the powers method (this input is optional)");
+            stringinput2 = Console.ReadLine();
+            if (string.IsNullOrEmpty(stringinput2))
+            {
+                Console.WriteLine("No second input");
+                Console.WriteLine("Base " + userinput1 + " to the power of 1: " + mathinstance.powers(userinput1));
+            }
+            else
+            {
+                Console.WriteLine("Second input received");
+                userinput2 = Convert.ToInt32(stringinput2);
+                Console.WriteLine("Base " + userinput1 + " to the power of " + userinput2 + " is: " + mathinstance.powers(userinput1, userinput2));
+            }
+
+            Console.WriteLine("\nEnd of the program, press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
