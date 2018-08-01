d71bc1cf9d90e941c85a6db92add2a9b49440b0f
RyanR <ruizry@oregonstate.edu>
2018-08-01 13:18:54 -0700

    Added inheritance demo

---
+++ inheritanceDemo/inheritanceDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmployee = new Employee();
+
+            newEmployee.FirstName = "Sample";
+            newEmployee.LastName = "Student";
+
+            newEmployee.SayName();
+
+            Console.WriteLine("\nEnd of the program, press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
