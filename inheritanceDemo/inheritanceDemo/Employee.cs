8b116c300c8ed9b3629c85a0fcd391aba98d2898
RyanR <ruizry@oregonstate.edu>
2018-08-07 18:08:18 -0700

    Added overloading == to inheritanceDemo

--- inheritanceDemo/inheritanceDemo/Employee.cs
+++ inheritanceDemo/inheritanceDemo/Employee.cs
@@ -8,11 +8,33 @@ namespace inheritanceDemo
 {
     class Employee : Person, IQuittable
     {
+        public Employee (string fname, string lname, int newid = 0)
+        {
+            this.Id = newid;
+            this.FirstName = fname;
+            this.LastName = lname;
+        }
+
         public int Id { get; set; } = 0;
 
         public void Quit()
         {
             Console.WriteLine(this.FirstName + " " + this.LastName + " has quit the company");
+        }
+
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            return ((emp1.FirstName == emp2.FirstName) && (emp1.LastName == emp2.LastName) && (emp1.Id == emp2.Id));
+        }
+
+        public static bool operator!= (Employee emp1, Employee emp2)
+        {
+            return ((emp1.FirstName != emp2.FirstName) || (emp1.LastName != emp2.LastName) || (emp1.Id != emp2.Id));
+        }
+
+        public bool Equals(Employee emp)
+        {
+            return ((emp.FirstName == this.FirstName) && (emp.LastName == this.LastName) && (emp.Id == this.Id));
         }
     }
 }
--- inheritanceDemo/inheritanceDemo/Program.cs
+++ inheritanceDemo/inheritanceDemo/Program.cs
@@ -10,15 +10,35 @@ namespace inheritanceDemo
     {
         static void Main(string[] args)
         {
-            Employee newEmployee = new Employee();
-
-            newEmployee.FirstName = "Sample";
-            newEmployee.LastName = "Student";
+            Employee newEmployee = new Employee("Sample", "Student");
 
             newEmployee.SayName();
 
             Console.WriteLine("\nCalling method Quit inherited from IQuittable");
             newEmployee.Quit();
+
+            Console.WriteLine("\nCreating three new employees to test overloaded == operator");
+
+            Employee testEmployee1 = new Employee("Roger", "Frank", 777);
+            Employee testEmployee2 = new Employee("Jimmy", "Lee");
+            Employee testEmployee3 = new Employee("Jimmy", "Lee");
+
+            Console.WriteLine("The following employees were added");
+            testEmployee1.SayName();
+            testEmployee2.SayName();
+            testEmployee3.SayName();
+
+            Console.WriteLine();
+
+            if (testEmployee1 == testEmployee2)
+                Console.WriteLine("testEmployee1: " + testEmployee1.FirstName + " with id: " + testEmployee1.Id + " is the same as " + "testEmployee2: " + testEmployee2.FirstName + " with id: " + testEmployee2.Id);
+            else
+                Console.WriteLine("testEmployee1: " + testEmployee1.FirstName + " with id: " + testEmployee1.Id + " is not the same as " + "testEmployee2: " + testEmployee2.FirstName + " with id: " + testEmployee2.Id);
+
+            if (testEmployee2 == testEmployee3)
+                Console.WriteLine("testEmployee2: " + testEmployee2.FirstName + " with id: " + testEmployee2.Id + " is the same as " + "testEmployee3: " + testEmployee3.FirstName + " with id: " + testEmployee3.Id);
+            else
+                Console.WriteLine("testEmployee2: " + testEmployee2.FirstName + " with id: " + testEmployee2.Id + " is not the same as " + "testEmployee3: " + testEmployee3.FirstName + " with id: " + testEmployee3.Id);
 
             Console.WriteLine("\nEnd of the program, press enter to exit");
             Console.ReadLine();
