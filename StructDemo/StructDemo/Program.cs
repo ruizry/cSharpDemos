88f9779048e7545c294a5cbad8d61f44ddf4f26a
RyanR <ruizry@oregonstate.edu>
2018-08-09 14:51:32 -0700

    Added struct demo

---
+++ StructDemo/StructDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a decimal value for creating a Number object");
+            decimal val = Convert.ToDecimal(Console.ReadLine());
+
+            Number newNum = new Number(val);
+
+            Console.WriteLine("\nNew Number object created with the following value: " + newNum.myDeci);
+
+            Console.WriteLine("\nEnd of the program, press enter to exit");
+            Console.ReadLine();
+        }
+
+        struct Number
+        {
+            public decimal myDeci { get; }
+            public Number(decimal newDeci)
+            {
+                myDeci = newDeci;
+            }
+        };
+    }
+}
