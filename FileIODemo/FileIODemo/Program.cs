5e5a2dedd6f52752d5c00fad57d46921c9eed667
RyanR <ruizry@oregonstate.edu>
2018-08-13 12:38:31 -0700

    Added IO Demo

---
+++ FileIODemo/FileIODemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FileIODemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to log to a file");
+            string number = Console.ReadLine();
+            File.WriteAllText(@".\log.txt", number);
+            Console.WriteLine("Number successfully logged\n\nNow reading log and outputting contents to console:");
+
+            string input = File.ReadAllText(@".\log.txt");
+            Console.WriteLine(input);
+
+            Console.WriteLine("\nEnd of the program, press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
